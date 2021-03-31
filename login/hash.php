4c696197d1574c76fec00d97f255077794d77e49
Jarrett Kuhn <55860277+jrt-kuhn@users.noreply.github.com>
2021-03-31 14:59:22 -0400

    Update hash.php

--- login/hash.php
+++ login/hash.php
@@ -1,1 +1,9 @@
-//placeholder for hash generation file
+<?php
+$password = "test";
+$a = password_hash($password, PASSWORD_BCRYPT);
+$b = password_hash($password, PASSWORD_BCRYPT);
+echo $a . "<br>";
+echo $b . "<br>";
+
+echo '$a == $b?' . ($a == $b?"true":"false");
+?>
